7a9eb2c5339ca9b915bd9dff4f5ff14813246045
Nitisarath <64013165+PluzNtp@users.noreply.github.com>
2022-10-02 04:24:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ import glob
 import re
 import fitz
 from transformers import pipeline
-
-
-st.write()
+A="<<<<< Click Here first before summarize"
+Download = st.button("DOWNLOAD EACH MODEL"),st.write(A)
 Summarizeit = st.button("CLICK TO SUMMARIZE")
 st.write("My pdf has 1 research per pdf || Better summarization and keyword graph)")
 Summarizebunch = st.button("CLICK TO SUMMARIZE(multiple research per pdf)")
 st.write("My pdf has multiple research per pdf")
 uploaded_files = st.file_uploader("Choose a PDF file", type=["pdf"] ,accept_multiple_files=True)
 
-
 #--- getting pre trained model
-fix_spelling = pipeline("text2text-generation",model="oliverguhr/spelling-correction-english-base")
-summarizer = pipeline('summarization')
+
+
 from transformers import (
     TokenClassificationPipeline,
     AutoModelForTokenClassification,
@@ -25,6 +23,8 @@ from transformers import (
 )
 from transformers.pipelines import AggregationStrategy
 import numpy as np
+
+# Define keyphrase extraction pipeline
 class KeyphraseExtractionPipeline(TokenClassificationPipeline):
     def __init__(self, model, *args, **kwargs):
         super().__init__(
@@ -40,8 +40,8 @@ class KeyphraseExtractionPipeline(TokenClassificationPipeline):
             aggregation_strategy=AggregationStrategy.SIMPLE,
         )
         return np.unique([result.get("word").strip() for result in results])
-model_name = "ml6team/keyphrase-extraction-kbir-inspec"
-extractor = KeyphraseExtractionPipeline(model=model_name)
+
+
 #-------------- 
 
 
@@ -51,6 +51,11 @@ if Summarizeit == True:
     i = 0  
     num_paper = 0
     alternate = 0
+    fix_spelling = pipeline("text2text-generation",model="oliverguhr/spelling-correction-english-base")
+    model_name = "ml6team/keyphrase-extraction-kbir-inspec"
+    extractor = KeyphraseExtractionPipeline(model=model_name)
+    summarizer = pipeline('summarization')
+
     for paper in uploaded_files:
         file = paper
         pdf_reader = pdf.PdfFileReader(file) #file reader
@@ -66,41 +71,46 @@ if Summarizeit == True:
                 corrected = fix_spelling(text1,max_length=4000)
                 correctedintro = corrected[0]['generated_text']
                 st.write("Introduction at page",i +1)
+                Intro = True
                 i = i +1 
                 num_paper = num_paper + 1
             elif "ABSTRACT" in text1 :
                 text1 = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text1)
                 corrected = fix_spelling(text1,max_length=4000)
                 correctedabs = corrected[0]['generated_text'] 
-                correctedintro = correctedintro + correctedabs 
                 st.write("Abstract at page",i +1)
                 i = i + 1
+                Abs = True
+                if Intro == True:
+                    correctedintro = correctedabs + correctedintro
+                else:
+                    correctedintro = correctedabs 
             elif "CONCLUSION" in text1 :
                 text1 = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text1)
                 corrected = fix_spelling(text1,max_length=4000)
                 correctecon = corrected[0]['generated_text']
-                correctedintro =  correctedintro + correctecon
                 st.write('Conclusion at page',i +1)
                 i = i + 1 
+                if Abs == True or Intro == True:
+                    correctedintro =  correctedintro + correctecon
+                else:
+                    correctedintro =  correctecon
             else:
                 i = i +1 
-summaryfin = summarizer(correctedintro,max_length = 100 , min_length = 50 ,do_sample = False)
-summaryfin = summaryfin[0]['summary_text'] 
-st.write('Path to Pdf File',paper)
-st.write("summary of research:",summaryfin)
-with fitz.open(paper) as doc:
-  text = ""
-  for page in doc:
-    text += page.get_text()
-    text = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
-  keyphrases = extractor(text)
-  for word in (keyphrases):
-    text.count(word)
-    st.write(word,"Appeared",text.count(word),"times")
+    summaryfin = summarizer(correctedintro,max_length = 70 , min_length = 30 ,do_sample = False)
+    summaryfin = summaryfin[0]['summary_text'] 
+    st.write('Path to Pdf File',paper)
+    st.write("summary of research:",summaryfin)
+    with fitz.open(paper) as doc:
+        text = ""
+        for page in doc:
+            text += page.get_text()
+            text = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", text)
+            keyphrases = extractor(text)
+            for word in (keyphrases):
+                text.count(word)
+                st.write(word,"Appeared",text.count(word),"times")
 
-st.write('-------------------------------------------------------------------------------------------------------------------------------------------')
-        
-
-
+    st.write('-------------------------------------------------------------------------------------------------------------------------------------------')
 
 
